7d69cde8b7a614f305e4edb53709ac75450b0c0d
muqtar <muqtarabdiaziz8@gmail.com>
2023-09-16 11:06:11 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+name=["Guadalupe","Ollie","Aki"];
+function writeCards(names) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+}
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const messages = writeCards(names);
+console.log(messages);
+writeCards()
+
+function countDown(number){
+    for(let i=number;i>=0;i--){
+        console.log(i)
+    }
+    return number
+}
+
+countDown(10)
